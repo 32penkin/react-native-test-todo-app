98c0acc202817ff712ce9a40f8e2d0b476c479ac
Yauhen <e.penkin@akveo.com>
2018-02-16 18:00:37 +0300

    feat(row): "todo" row add

--- app/app.js
+++ app/app.js
@@ -1,32 +1,39 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Platform, ListView, Keyboard } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Platform, ListView, Keyboard} from 'react-native';
 import Header from './header';
 import Footer from './footer';
+import Row from './row';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       allComplete: false,
       value: '',
-      items: []
+      items: [],
+      dataSource: ds.cloneWithRows([])
     };
 
     this.handleAddItem = this.handleAddItem.bind(this);
     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+    this.setSource = this.setSource.bind(this);
   }
 
+  setSource(items, itemsDataSource, otherState = {}) {
+    this.setState({
+      items,
+      dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
+      ...otherState
+    })
+  }
   handleToggleAllComplete() {
     const complete = !this.state.allComplete;
     const newItems = this.state.items(item => ({
       ...item,
       complete,
     }));
-    console.table(newItems);
-    this.setState({
-      items: newItems,
-      allComplete: complete,
-    });
+    this.setSource(newItems, newItems, {allComplete: complete});
   }
 
   handleAddItem() {
@@ -42,22 +49,41 @@ class App extends Component {
         complete: false,
       }
     ];
-    this.setState({
-      items: newItems,
-      value: '',
-    });
+    this.setSource(newItems, newItems, { value: '' });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Header
-        value={this.state.value}
-        onAddItem={this.handleAddItem}
-        onChange={(value) => this.setState({ value })}
-        onToggleAllComplete={this.handleToggleAllComplete}
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={(value) => this.setState({value})}
+          onToggleAllComplete={this.handleToggleAllComplete}
         />
         <View style={styles.content}>
+          <ListView
+            style={styles.list}
+            enableEmptySections
+            dataSource={this.state.dataSource}
+            onScroll={() => Keyboard.dismiss()}
+            renderRow={({key, ...value}) => {
+              return (
+                <Row
+                  key={key}
+                  { ...value }
+                />
+              );
+            }}
+            renderSeparator={(sectionId, rowId) => {
+              return (
+                <View
+                  key={rowId}
+                  style={styles.separator}
+                />
+              );
+            }}
+          />
         </View>
         <Footer/>
       </View>
@@ -70,11 +96,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5F5F5',
     ...Platform.select({
-      ios: { paddingTop: 30 },
+      ios: {paddingTop: 30},
     }),
   },
   content: {
     flex: 1,
+  },
+  list: {
+    backgroundColor: '#FFF',
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: '#F5F5F5'
   }
 });
 
