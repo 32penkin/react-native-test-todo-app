4e56095c96cf0d4d9a58c0eec65fc5d42647bc47
Yauhen <e.penkin@akveo.com>
2018-02-16 19:56:29 +0300

    feat(common): async-storage add

--- app/app.js
+++ app/app.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Platform, ListView, Keyboard } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  ListView,
+  Keyboard,
+  AsyncStorage
+} from 'react-native';
 import Header from './header';
 import Footer from './footer';
 import Row from './row';
@@ -33,12 +41,24 @@ class App extends Component {
     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
   }
 
+  componentWillMount() {
+    AsyncStorage.getItem('items').then(json => {
+      try {
+        const items = JSON.parse(json);
+        this.setSource(items, items);
+      } catch (e) {
+
+      }
+    })
+  }
+
   setSource(items, itemsDatasource, otherState = {}) {
     this.setState({
       items,
       dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
       ...otherState
-    })
+    });
+    AsyncStorage.setItem('items', JSON.stringify(items));
   }
 
   handleFilter(filter) {
