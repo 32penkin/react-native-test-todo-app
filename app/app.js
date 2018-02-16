ee6e0ad75a4cf7d5ea404e58d7afddb8bc00fe74
Yauhen <e.penkin@akveo.com>
2018-02-16 16:08:15 +0300

    feat(header): text-input for todos add

--- app/app.js
+++ app/app.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import Header from './header';
 import Footer from './footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      items: []
+    };
+
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+
+  handleAddItem() {
+    if (!this.state.value) {
+      return;
+    }
+
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false,
+      }
+    ];
+    this.setState({
+      items: newItems,
+      value: '',
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Header/>
-        <View>App!</View>
+      <View style={styles.container}>
+        <Header
+        value={this.state.value}
+        onAddItem={this.handleAddItem}
+        onChange={(value) => this.setState({ value })}
+        />
+        <View style={styles.content}>
+        </View>
         <Footer/>
       </View>
     );
@@ -16,7 +50,16 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    ...Platform.select({
+      ios: { paddingTop: 30 },
+    }),
+  },
+  content: {
+    flex: 1,
+  }
 });
 
 export default App;
